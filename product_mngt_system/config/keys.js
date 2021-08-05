ba45d1790875839f5ba08c6b5e5f83e09fa75230
shlokmehta21 <63841117+shlokmehta21@users.noreply.github.com>
2021-08-05 23:01:16 +0530

    Delete product_mngt_system directory

--- product_mngt_system/config/keys.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    MongoURI: 'mongodb+srv://root:root123@projectdb.aeaab.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-}
--- product_mngt_system/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { ensureAuthenticated } = require('../config/auth');
-
-//Welcome page
-router.get('/', (req, res) => res.render('welcome'));
-
-module.exports = router; 
